440402c912f743f6b5836ad29edefa2fd3fb26de
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-03 16:58:34 +0000

    StringConcat 1.2

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,9 +1,10 @@
-// package StringConcat;
+package StringConcat;
 
 public class StringConcat{
     public static String concat(String s1, String s2){
         if (s1 != null ) {
             return s1.concat(s2);
         }
+        return null;
     }
 }
