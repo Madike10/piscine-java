c4918b548f022d24385de47b6fff9f53729d6003
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-03 19:29:53 +0000

    StringContains 1.2

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -2,9 +2,8 @@
 
 public class StringContains{
     public static boolean isStringContainedIn(String subString, String s){
-        if(s == null){
-            return false;
-        }
+         if (s == null) return false;
+         if (subString == null) return false;
         return s.contains(subString);
     }
 }
