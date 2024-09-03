986f6155088b00bbfed5513449d81a7f1e700e51
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-03 16:24:48 +0000

    StringLength 1.1

---
+++ StringLength/StringLengthRunner.java
@@ -0,0 +1,7 @@
+package StringLength;
+public class StringLengthRunner {
+    public static void main(String[] args) {
+        System.out.println(StringLength.getStringLength("Hello World !"));
+        System.out.println(StringLength.getStringLength(""));
+    }
+}
