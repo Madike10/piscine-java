a15870cc233e337927d476e5bc182c7e84fb3bea
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-03 22:52:30 +0000

    StringReplace 1.0

---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,10 @@
+// package StringReplace;
+
+public class StringReplace {
+    public static String replace(String s, String target, String replacement) {
+        if (s == null || target == null || replacement == null) {
+            return s;
+        }
+        return s.replace(target, replacement);
+    }
+}
