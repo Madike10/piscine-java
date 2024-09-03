72407e50a1d9fb0fb317434e96717eca8c393d55
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-03 17:00:35 +0000

    StringConcat 1.4

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,7 +2,7 @@
 
 public class StringConcat{
     public static String concat(String s1, String s2){
-        if (s1 != null ) {
+        if (s1 != null || s2 != null) {
             return s1.concat(s2);
         }
         return null;
