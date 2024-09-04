b9bdd9cd6b2fc79304586eec2a7145f0d1bc0bba
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-04 13:02:02 +0000

    DoOp 1.4

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -3,26 +3,22 @@
 public class DoOp {
     public static String operate(String[] args) {
         int result = 0;
-        if (args.length != 3){
-            return "it depend on the input.";
-        }
-        if (!args[1].equals("+") && !args[1].equals("-") && !args[1].equals("%")){
+        if (args == null || args.length < 3) {
             return "Error";
         }
-        if (args.length == 0){
+        if (args.length == 0) {
             return "Error";
-        }
-        if (args[1] == "+"){
+        } else if (args[1] == "+") {
             result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
-        }else if (args[1] == "-"){
+        } else if (args[1] == "-") {
             result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
-        }else if ((args[1] == "%")){
-            if (args[2] != "0"){
+        } else if ((args[1] == "%")) {
+            if (args[2] != "0") {
                 result = Integer.parseInt(args[0]) % Integer.parseInt(args[2]);
-            }else{
+            } else {
                 return "Error";
             }
-        }else if (args[1] == "*"){
+        } else if (args[1] == "*") {
             result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
         }
         return String.valueOf(result);
