925275ebb0d549e87887632118086c351f2ef284
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-04 17:46:42 +0000

    Cat 1.1

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -1,24 +1,24 @@
-// package Cat;
+package Cat;
 
 import java.io.*;
 
+
 public class Cat {
     public static void cat(String[] args) throws IOException {
-        for (String file : args) {
-            try {
-                File f = new File(file);
-                if (!f.exists()) {
-                    throw new FileNotFoundException("File '" + file + "' not found.");
-                }
+        if (args.length < 1) {
+            return;
+        }
 
-                try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f))) {
-                    int byteRead;
-                    while ((byteRead = bis.read()) != -1) {
-                        System.out.write(byteRead);
-                    }
+        for (String fileName : args) {
+            try (FileInputStream fis = new FileInputStream(fileName);
+                 BufferedInputStream bis = new BufferedInputStream(fis)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = bis.read(buffer)) != -1) {
+                    System.out.write(buffer, 0, bytesRead);
                 }
-            } catch (FileNotFoundException e) {
-                System.err.println(e.getMessage());
+            } catch (IOException e) {
+                System.err.println("Error reading file: " + fileName);
             }
         }
     }
