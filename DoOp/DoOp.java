e8c43a7dde6d7b3272922b88acba7c41b14f5681
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-04 13:05:20 +0000

    DoOp 1.4

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -6,21 +6,24 @@ public class DoOp {
         if (args == null || args.length < 3) {
             return "Error";
         }
-        if (args.length == 0) {
-            return "Error";
-        } else if (args[1] == "+") {
-            result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
-        } else if (args[1] == "-") {
-            result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
-        } else if ((args[1] == "%")) {
-            if (args[2] != "0") {
-                result = Integer.parseInt(args[0]) % Integer.parseInt(args[2]);
-            } else {
+        if (args.length == 3){
+            if (args.length == 0) {
                 return "Error";
+            } else if (args[1] == "+") {
+                result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+            } else if (args[1] == "-") {
+                result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+            } else if ((args[1] == "%")) {
+                if (args[2] != "0") {
+                    result = Integer.parseInt(args[0]) % Integer.parseInt(args[2]);
+                } else {
+                    return "Error";
+                }
+            } else if (args[1] == "*") {
+                result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
             }
-        } else if (args[1] == "*") {
-            result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+            return String.valueOf(result);
         }
-        return String.valueOf(result);
+        return "Error";
     }
 }
