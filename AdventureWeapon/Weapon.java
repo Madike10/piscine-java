25121750df16731cbdd007499659582e044a630d
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-12 01:37:32 +0000

    AdventureWeapon 1.7

--- AdventureWeapon/Weapon.java
+++ AdventureWeapon/Weapon.java
@@ -10,6 +10,12 @@ public class Weapon{
     public int getDamage(){
         return damage;
     }
+    public void setDamage(int damage){
+        this.damage = damage;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
     public Weapon(String name, int damage){
         this.name = name;
         this.damage = damage;
