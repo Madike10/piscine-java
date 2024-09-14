a96aba03a14a588e7e083266650c136732016931
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-14 16:51:13 +0000

    ListContains 1.0

---
+++ ListContains/ListContains.java
@@ -0,0 +1,10 @@
+// package ListContains;
+
+import java.util.List;
+
+public class ListContains {
+    public static boolean containsValue(List<Integer> list, Integer value) {
+
+        return (list.contains(value)) ? true : false;
+    }
+}
