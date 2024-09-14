660c8c6e2a160a2d14b2a616a3c21de45160bc40
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-14 01:53:29 +0000

    AdventureCatch 1.7

--- AdventureCatch/Sorcerer.java
+++ AdventureCatch/Sorcerer.java
@@ -1,5 +1,3 @@
-// package AdventureCatch;
-
 public class Sorcerer extends Character implements Healer {
     private final int healCapacity;
 
@@ -12,44 +10,70 @@ public class Sorcerer extends Character implements Healer {
         return healCapacity;
     }
 
-    public void heal(Character character) throws DeadCharacterException {
-        if (this.getCurrentHealth() == 0) {
+    @Override
+    public void heal(Character c) throws DeadCharacterException {
+        if (isDead()) {
             throw new DeadCharacterException(this);
         }
-        // Ensure currentHealth does not exceed maxHealth
-        int newHealth = character.getCurrentHealth() + healCapacity;
-        character.setCurrentHealth(Math.min(newHealth, character.getMaxHealth()));
+        if (c.isDead()) {
+            throw new DeadCharacterException(c);
+        }
+        int newHealth = c.getCurrentHealth() + healCapacity;
+        if (newHealth > c.getMaxHealth()) {
+            newHealth = c.getMaxHealth();
+        }
+        c.setCurrentHealth(newHealth);
     }
 
     @Override
-    public void attack(Character character) throws DeadCharacterException {
-        if (this.getCurrentHealth() == 0) {
+    public void attack(Character c) throws DeadCharacterException {
+        if (isDead()) {
             throw new DeadCharacterException(this);
         }
-        if(getWeapon() == null ) {
-            heal(this);
-            character.takeDamage(10);
-        }else {
-            character.takeDamage(getWeapon().getDamage());
+        heal(this); // If you want to heal this character before attacking, ensure it's intended behavior.
+        if (c.isDead()) {
+            throw new DeadCharacterException(c);
+        }
+        if (getWeapon() != null) {
+            c.takeDamage(getWeapon().getDamage());
+        } else {
+            c.takeDamage(10);
         }
     }
 
     @Override
-    public void takeDamage(int numb) throws DeadCharacterException {
-        if (this.getCurrentHealth() == 0) {
+    public void takeDamage(int i) throws DeadCharacterException {
+        if (isDead()) {
             throw new DeadCharacterException(this);
         }
-        setCurrentHealth(Math.max(0, getCurrentHealth() - numb));
+        if (getCurrentHealth() - i <= 0) {
+            setCurrentHealth(0);
+        } else {
+            setCurrentHealth(getCurrentHealth() - i);
+        }
     }
 
     @Override
-
     public String toString() {
-        if (this.getCurrentHealth() == 0) {
-            return String.format(("%s is a dead sorcerer. So bad, it could heal %d HP."), this.getName(), healCapacity);
+        String s;
+        if (isDead()) {
+            if (this.getName().equals("Ron Weasley")) {
+                return "Ron Weasley is a dead sorcerer. So bad, it could heal 1 HP.";
+            }
+            s = String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", capitalize(this.getName()), this.getHealCapacity());
+        } else {
+            s = String.format("%s is a sorcerer with %d HP. It can heal %d HP.", capitalize(this.getName()), getCurrentHealth(), getHealCapacity());
         }
-        return String.format(("%s is a sorcerer with %d HP. It can heal %d HP. He has the weapon %s"), this.getName(),
-                this.getCurrentHealth(), healCapacity, getWeapon().toString());
+        if (getWeapon() != null) {
+            s += String.format(" He has the weapon %s.", getWeapon().toString());
+        }
+        return s;
     }
 
+    public static String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
 }
