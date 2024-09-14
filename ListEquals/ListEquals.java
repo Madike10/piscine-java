59833d5a6be125aae607f282879f024454ff6077
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-14 17:13:45 +0000

    ListEquals 1.8

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -1,14 +1,14 @@
 // package ListEquals;
 
 import java.util.List;
-
 public class ListEquals {
-    public static Boolean areListsEqual(List<String> list1, List<String> list2) {
-        if ((list1 == null || list2 == null)) {
-            return false;
-        } else if (list1.isEmpty() || list2.isEmpty()) {
-            return false;
-        }
-        return list1.equals(list2);
+    public static boolean areListsEqual(List<String> list1, List<String> list2) {
+         if (list1 == null && list2 == null) {
+              return true;
+         }
+         if (list1 == null || list2 == null) {
+              return false;
+         }
+         return list1.equals(list2);
     }
 }
