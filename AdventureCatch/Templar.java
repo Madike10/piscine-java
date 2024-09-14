1ad4dd6767770f8ee7d78f29321ffae02c124d49
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-14 01:35:20 +0000

    AdventureCatch 1.3

--- AdventureCatch/Templar.java
+++ AdventureCatch/Templar.java
@@ -19,15 +19,7 @@ public class Templar extends Character implements Tank, Healer {
         return healCapacity;
     }
 
-    // Implémentation de la méthode heal de l'interface Healer
-    @Override
-    public void heal(Character character) throws DeadCharacterException {
-        if (this.getCurrentHealth() == 0) {
-            throw new DeadCharacterException(this);
-        }
-        int newHealth = character.getCurrentHealth() + healCapacity;
-        character.setCurrentHealth(Math.min(newHealth, character.getMaxHealth()));
-    }
+
 
     // Implémentation de la méthode getShield de l'interface Tank
     @Override
@@ -44,25 +36,29 @@ public class Templar extends Character implements Tank, Healer {
         return getName() + " is a strong Templar with " + getCurrentHealth() + " HP. It can heal " + healCapacity + " HP and has a shield of " + shield + "." + " He has the weapon " + getWeapon().toString();
     }
     @Override
-    public void attack(Character character) throws DeadCharacterException{
-        if (this.getCurrentHealth() == 0) {
-            throw new DeadCharacterException(this);
-        }
+    public void attack(Character character) throws DeadCharacterException {
         heal(this);
-        if(getWeapon() == null ) {
-            character.takeDamage(6);
-        }else {
-            character.takeDamage(getWeapon().getDamage());
+        character.takeDamage(6);
+        if (character.getCurrentHealth() <= 0) {
+            throw new DeadCharacterException(this);
         }
     }
 
     @Override
-    public void takeDamage(int damage) throws DeadCharacterException{
-        if (this.getCurrentHealth() == 0) {
+    public void takeDamage(int damage) throws DeadCharacterException {
+        int effectiveDamage = Math.max(0, damage - shield);
+        setCurrentHealth(Math.max(0, getCurrentHealth() - effectiveDamage));
+        if (getCurrentHealth() <= 0) {
             throw new DeadCharacterException(this);
         }
-        
-        int effectiveDamage = Math.min(0, damage - shield);
-        setCurrentHealth(Math.max(0, getCurrentHealth() - effectiveDamage));
+    }
+
+    @Override
+    public void heal(Character character) throws DeadCharacterException {
+        int newHealth = character.getCurrentHealth() + healCapacity;
+        character.setCurrentHealth(Math.min(newHealth, character.getMaxHealth()));
+        if (character.getCurrentHealth() <= 0) {
+            throw new DeadCharacterException(this);
+        }
     }
 }
