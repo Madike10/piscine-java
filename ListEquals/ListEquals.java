fa8cc01759f6d5f5a8c74277edde06d58949c071
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-14 17:08:37 +0000

    return list1.equals(list2);

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -1,10 +1,13 @@
 // package ListEquals;
 
 import java.util.List;
-public class ListEquals{
-    public static Boolean areListsEqual(List<String> list1, List<String> list2){
-        if ((list1 == null || list2 == null)  ){
+
+public class ListEquals {
+    public static Boolean areListsEqual(List<String> list1, List<String> list2) {
+        if ((list1 == null || list2 == null)) {
             return false;
+        } else if (list1.isEmpty() && list2.isEmpty()) {
+            return list1.equals(list2);
         }
         return list1.equals(list2);
     }
