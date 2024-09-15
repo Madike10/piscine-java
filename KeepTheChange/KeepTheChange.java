ff8c32671657d00da862521637e3eca038b041ca
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-09-15 03:16:24 +0000

    KeepTheChange 1.1

--- KeepTheChange/KeepTheChange.java
+++ KeepTheChange/KeepTheChange.java
@@ -1,16 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.ArrayList;
-
 public class KeepTheChange {
-    public static List<Integer> computeChange(int amount, Set<Integer> coins) {
-        List<Integer> change = new ArrayList<>();
-        for (int coin : coins) {
-            while (amount >= coin) {
-                change.add(coin);
-                amount -= coin;
-            }
-        }
-        return change;
-    }
+     public static List<Integer> computeChange(int amount, Set<Integer> coins) {
+          List<Integer> result = new ArrayList<>();
+          List<Integer> sortedCoins = new ArrayList<>(coins);
+          Collections.sort(sortedCoins, Collections.reverseOrder());
+          for (int coin : sortedCoins) {
+               while (amount >= coin) {
+                    amount -= coin;
+                    result.add(coin);
+               }
+          }
+          return result;
+     }
 }
